e31caf4af6e791743b66dd7972b40499f70b28f9
gssa-13 <gssa-13@protonmail.com>
2021-11-24 19:35:15 -0600

    Son agregadas traducciones * Son agregadas las traducciones de espanol en laravel * son realizados cambios en el layout para la vista administrativa del blog

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
     $posts = Post::latest('published_at')->get();
     return view('welcome', compact('posts'));
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
