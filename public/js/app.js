88b0f82bee0743de8416617ab14d2071d07c850a
gssa-13 <gssa-13@protonmail.com>
2021-12-21 23:54:45 -0600

    The hash <<#>> was removed from url

--- public/js/app.js
+++ public/js/app.js
@@ -6074,7 +6074,7 @@ __webpack_require__.r(__webpack_exports__);
 vue__WEBPACK_IMPORTED_MODULE_0__["default"].use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
   routes: [{
-    path: '/home',
+    path: '/',
     name: 'home',
     component: (__webpack_require__(/*! ./blog/Home.vue */ "./resources/js/blog/Home.vue")["default"])
   }, {
@@ -6109,7 +6109,8 @@ vue__WEBPACK_IMPORTED_MODULE_0__["default"].use(vue_router__WEBPACK_IMPORTED_MOD
     component: (__webpack_require__(/*! ./blog/404.vue */ "./resources/js/blog/404.vue")["default"])
   }],
   linkExactActiveClass: 'header-active',
-  linkActiveClass: 'active-route'
+  linkActiveClass: 'active-route',
+  mode: 'history'
 }));
 
 /***/ }),
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
-        { path: '/home', name: 'home' ,component: require('./blog/Home.vue').default },
+        { path: '/', name: 'home' ,component: require('./blog/Home.vue').default },
         { path: '/about', name: 'about' ,component: require('./blog/About.vue').default },
         { path: '/archive',name: 'archive' , component: require('./blog/Archive.vue').default },
         { path: '/contact', name: 'contact', component: require('./blog/Contact.vue').default },
@@ -17,5 +17,6 @@ export default new VueRouter({
         { path: '*', component: require('./blog/404.vue').default },
     ],
     linkExactActiveClass: 'header-active',
-    linkActiveClass: 'active-route'
+    linkActiveClass: 'active-route',
+    mode: 'history'
 });
